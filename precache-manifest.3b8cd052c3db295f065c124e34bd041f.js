1f996fea9e8b02a0d7c0da0eb9bf291aa15aa963
wouterdeen <wouter.deen@icloud.com>
2020-07-21 13:23:17 +0200

    Updates

---
+++ precache-manifest.3b8cd052c3db295f065c124e34bd041f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb88ba89bbe42283fc60c06da479c197",
+    "url": "/schoolcatalog/index.html"
+  },
+  {
+    "revision": "101726866971aa4b31c5",
+    "url": "/schoolcatalog/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2923b1bd270bbf5698d7",
+    "url": "/schoolcatalog/static/js/2.313dbd98.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/schoolcatalog/static/js/2.313dbd98.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "101726866971aa4b31c5",
+    "url": "/schoolcatalog/static/js/main.8cb08e43.chunk.js"
+  },
+  {
+    "revision": "a6b58407c1ed484ac5cd",
+    "url": "/schoolcatalog/static/js/runtime-main.1d27b30d.js"
+  }
+]);
