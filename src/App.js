97dd51bfe022c6cddfeed90cc5352640759b3d27
wouterdeen <wouter.deen@icloud.com>
2020-07-22 21:23:32 +0200

    hashrouter test

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import {BrowserRouter as Router, Route} from 'react-router-dom'
+import {HashRouter as Router, Route} from 'react-router-dom'
 import Homepage from './components/Homepage/Homepage'
 import Natuurkunde from './components/School/Vakken/Natuurkunde/Natuurkunde'
 import Scheikunde from './components/School/Vakken/Scheikunde/Scheikunde'
